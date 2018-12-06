4cb565ff8f4ae84ab3d3b02a7eee26c5b520caef
MarSchlaefer <mschlaefer00@gmail.com>
2018-12-06 13:11:30 -0500

    Changed play formatting.

--- src/components/admin/play.js
+++ src/components/admin/play.js
@@ -21,7 +21,33 @@ export default class Play extends Component {
   formatPlay = () => {
     const player = this.findPlayer(this.props.play.player_id)
     if (player) {
-      return `${this.props.play.action} - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      if (this.props.play.action === "J") {
+        return `Jump shot - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "L") {
+        return `Layup - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "D") {
+        return `Dunk - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "Y") {
+        return `Three Pointer - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "R") {
+        return `Rebound - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "T") {
+        return `Turnover - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "S") {
+        return `Steal - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "A") {
+        return `Assist - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "E") {
+        return `Free throw - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "F") {
+        return `Foul - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "K") {
+        return `Block - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else if (this.props.play.action === "O") {
+        return `Timeout - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      } else {
+        return `${this.props.play.action} - #${player.number} - ${player.name} is ${this.props.play.result}.`
+      }
     } else {
       return ''
     }
